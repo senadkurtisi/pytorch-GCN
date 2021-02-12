e8180763e441f643c96b068c52d2ca9879a4a3b7
DESKTOP-18SEH2D\Senad <senad98kurtisi@gmail.com>
2021-02-12 20:27:13 +0100

    Implemented GCN model and train/val/test split

---
+++ src/model.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GCNLayer(nn.Module):
+    def __init__(self, in_features, out_features, use_bias=True):
+        super(GCNLayer, self).__init__()
+        self.weight = nn.Parameter(torch.FloatTensor(torch.zeros(size=(in_features, out_features))))
+        if use_bias:
+            self.bias = nn.Parameter(torch.FloatTensor(torch.zeros(size=(out_features, ))))
+        else:
+            self.register_parameter('bias', None)
+
+    def initialize_weights(self):
+        nn.init.xavier_uniform_(self.layer.weight)
+        if self.bias is not None:
+            nn.init.zeros_(self.layer.bias)
+
+    def forward(self, x, adj):
+        x = x @ self.weight
+        if self.bias is not None:
+            x += self.bias
+
+        return torch.sparse.mm(adj, x)
+
+
+class GCN(nn.Module):
+    def __init__(self, node_features, hidden_dim, num_classes, dropout, use_bias=True):
+        super(GCN, self).__init__()
+        self.gcn_1 = GCNLayer(node_features, hidden_dim, use_bias)
+        self.gcn_2 = GCNLayer(hidden_dim, num_classes, use_bias)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x, adj):
+        x = F.relu(self.gcn_1(x, adj))
+        x = self.dropout(x)
+        x = self.gcn_2(x, adj)
+        return x
